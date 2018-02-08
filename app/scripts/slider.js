920fb82f1cac88a8f38d333ab80ff00119b94d78
TheAshen1 <theexaltedmax@gmail.com>
2018-02-08 11:43:01 +0200

    carousel

--- app/scripts/slider.js
+++ app/scripts/slider.js
@@ -1,3 +1,18 @@
 module.exports = function() {
-    console.log('hi');
+	$(document).ready(function(){
+		$(".owl-carousel").owlCarousel({
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:4
+				}
+			}
+		});
+	});
+
 }
