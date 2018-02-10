5c209524d1f79a4d9b40105c2d603983d7f8ba1c
casper <ycasperinho@gmail.com>
2018-02-10 15:51:35 +0200

    email saving

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const models = require('./models');
 const router  = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+
 
 router.get('/', function(req, res) {
   models.Event.findAll({
@@ -11,6 +14,42 @@ router.get('/', function(req, res) {
       events: events
     });
   });
+});
+
+router.post('/subscribe', [
+    check('email')
+        .isEmail()
+        .withMessage('must be a valid email')
+        .trim()
+        .normalizeEmail()
+        .custom(requestEmail => {
+          return models.User.findOne({
+              where: {
+                  email: requestEmail
+              }
+          }).then(user => {
+            if (user)
+                throw new Error('this email is already in use');
+          })
+        }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.mapped() });
+    }
+
+    models.User.findOrCreate({
+        where: {
+            email: req.body.email
+        },
+        defaults: {
+            name: req.body.name
+        }
+    }).then(() => {
+        res.redirect('/');
+    }).error((err) => {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
