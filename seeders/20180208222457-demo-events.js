bb2a61c421dcc6edfbcb478866e3aa8c8f1437e1
kusamops <katy.bas@mail.ru>
2018-02-10 02:42:32 +0200

    translate text into ua

--- seeders/20180208222457-demo-events.js
+++ seeders/20180208222457-demo-events.js
@@ -15,37 +15,37 @@ module.exports = {
       return queryInterface.bulkInsert('Events', [
         {
           name: '1 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis, nobis similique. Dolorum assumenda facere inventore, voluptates iste veritatis aut quae nisi voluptatibus, aspernatur a magni quasi, illum eveniet, nam?',
+          description: 'Цю подію не забути ніколи ... Наш університет творить чудеса!',
           date: (new Date(2048, 10, 8)).toMysqlFormat(),
           picture: "http://www.immigrationnews.ca/wp-content/uploads/2017/07/students-retention-atlantic-canada.jpg"
         },
         {
           name: '2 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis, nobis similique. Dolorum assumenda facere inventore, voluptates iste veritatis aut quae nisi voluptatibus, aspernatur a magni quasi, illum eveniet, nam?',
+          description: 'Цю подію не забути ніколи ... Наш університет творить чудеса!',
           date: (new Date(2048, 10, 8)).toMysqlFormat(),
           picture: "http://www.ivg.it/photogallery_new/images/2018/01/patetta-euro-students-award-443473.660x368.jpg"
         },
         {
           name: '3 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis, nobis similique. Dolorum assumenda facere inventore, voluptates iste veritatis aut quae nisi voluptatibus, aspernatur a magni quasi, illum eveniet, nam?',
+          description: 'Супер подія у супер місці. Наш університет творить чудеса!',
           date: (new Date(2048, 10, 8)).toMysqlFormat(),
           picture: "https://si.wsj.net/public/resources/images/WE-AB093_EARNIN_P_20160323143657.jpg"
         },
         {
           name: '4 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis, nobis similique. Dolorum assumenda facere inventore, voluptates iste veritatis aut quae nisi voluptatibus, aspernatur a magni quasi, illum eveniet, nam?',
+          description: 'Магістерська програма навчання розпочата. Відбулося урочисте відкриття',
           date: (new Date(2048, 10, 8)).toMysqlFormat(),
           picture: "http://i.huffpost.com/gen/785659/thumbs/o-COLLEGE-TOURS-facebook.jpg"
         },
         {
           name: '5 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt veritatis sunt aliquam pariatur similique sint sit saepe. Quia dicta commodi nobis inventore numquam atque accusamus itaque facere, debitis architecto provident.',
+          description: 'Студенти відвідали ботанічний сад ... Наш університет творить чудеса!',
           date: (new Date(2011, 10, 7)).toMysqlFormat(),
           picture: "https://www.gapyear.com/images/advertiser_files/13976378039_305a9b9967_o.jpg"
         },
         {
           name: '6 подiя',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque assumenda sapiente est obcaecati distinctio facere dolorem pariatur suscipit, quas libero ipsum. Eius saepe autem, voluptatibus veritatis quae explicabo nisi soluta.',
+          description: 'Цю подію не забути ніколи ... Наш університет творить чудеса!',
           date: (new Date(2023, 8, 7)).toMysqlFormat(),
           picture: "https://www.qualquarter.co.za/wp-content/uploads/2015/06/3_student-group.jpg"
         }], {});
